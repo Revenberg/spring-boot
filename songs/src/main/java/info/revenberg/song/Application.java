fe16448fe443e8330a05c0922d3bb1a7ee7ef159
Sander Revenberg <sander@revenberg.info>
2020-02-16 15:25:59 +0100

    .

--- songs/src/main/java/info/revenberg/song/Application.java
+++ songs/src/main/java/info/revenberg/song/Application.java
@@ -18,7 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableAutoConfiguration // Sprint Boot Auto Configuration
 //@ComponentScan(basePackages = "info.revenberg.song")
-@ComponentScan(basePackages = { "info.revenberg.song", "info.revenberg.domain" })
+@ComponentScan(basePackages = "info.revenberg.*")
 @ConfigurationPropertiesScan("info.revenberg.song.properties")
 @EnableJpaRepositories("info.revenberg.song.dao.jpa") // To segregate Sqlite and JPA repositories.
 public class Application extends SpringBootServletInitializer {
