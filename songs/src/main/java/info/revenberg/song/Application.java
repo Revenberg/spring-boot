5bbcb0071728f114e72e5cc95bf9aaf707171eef
Sander Revenberg <sander@revenberg.info>
2020-02-20 22:11:16 +0100

    .

--- songs/src/main/java/info/revenberg/song/Application.java
+++ songs/src/main/java/info/revenberg/song/Application.java
@@ -1,11 +1,14 @@
 package info.revenberg.song;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,9 +18,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 
 @SpringBootApplication
-//@EnableAutoConfiguration // Sprint Boot Auto Configuration
+@Configuration
+@EnableAutoConfiguration // Sprint Boot Auto Configuration
 @ComponentScan(basePackages = { "info.revenberg.song" } )
-//@ConfigurationPropertiesScan("info.revenberg.song.properties")
+@ConfigurationPropertiesScan("info.revenberg.song.properties")
 @EnableJpaRepositories( "info.revenberg.song.dao.jpa" ) // To segregate Sqlite and JPA repositories.
 @EntityScan(basePackages = { "info.revenberg.domain" })
 
