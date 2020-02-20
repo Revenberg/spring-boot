7c3d60711e69007d3daa67bc382a0127d15516dd
Sander Revenberg <sander@revenberg.info>
2020-02-20 20:11:17 +0100

    .

--- songs/src/main/java/info/revenberg/song/Application.java
+++ songs/src/main/java/info/revenberg/song/Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,6 +24,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ConfigurationPropertiesScan("info.revenberg.song.properties")
 @EnableJpaRepositories( "info.revenberg.song.dao.jpa" ) // To segregate Sqlite and JPA repositories.
 @EntityScan(basePackages = { "info.revenberg.domain" })
+@PropertySources({
+    @PropertySource("classpath:application.yml"),
+    @PropertySource("bootstrap.properties")})
+
 public class Application extends SpringBootServletInitializer {
 
     private static final Class<Application> applicationClass = Application.class;
