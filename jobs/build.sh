3632fb0a9c0e9bacdccc78c4afbab9bf6777f93b
Sander Revenberg <sander@revenberg.info>
2020-02-23 16:14:38 +0100

    .

--- jobs/build.sh
+++ jobs/build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 process=`ps -ef | grep -v awk | awk -e '/java.*/ { print $2 }'`
-kill ${process} 2>/dev/null
+kill ${process}
 sleep 5s
 process=`ps -ef | grep -v awk | awk -e '/java.*/ { print $2 }'`
-kill ${process} 2>/dev/null
+kill ${process}
+
 
 cd ~
 mkdir ~/songs/db -p 2>/dev/null 2>/dev/null
@@ -16,16 +17,18 @@ echo configuration-service
 rm -rf ~/configuration-service
 git clone https://github.com/Revenberg/configuration-service.git
 cd ~/configuration-service
-mvn clean install package
+mvn clean package
 nohup mvn spring-boot:run &
+
 cd ~
+
 
 echo build spring-boot
 rm -rf ~/spring-boot
 git clone https://github.com/Revenberg/spring-boot.git
 
 cd ~/spring-boot
-mvn clean package
+mvn clean install
 
 cp ~/spring-boot/songs/src/main/resources/bootstrap.properties ~/spring-boot/songs/
 cp ~/spring-boot/songs/src/main/resources/application.yml      ~/spring-boot/songs/
