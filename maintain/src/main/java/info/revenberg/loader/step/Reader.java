99dfb63f95780ec6d1020605316d5c87bc4c7ccb
Sander Revenberg <sander@revenberg.info>
2020-02-23 22:09:56 +0100

    .

--- maintain/src/main/java/info/revenberg/loader/step/Reader.java
+++ maintain/src/main/java/info/revenberg/loader/step/Reader.java
@@ -3,21 +3,30 @@ package info.revenberg.loader.step;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.HttpMethod;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import info.revenberg.domain.Vers;
 import info.revenberg.loader.objects.DataObject;
+import info.revenberg.loader.objects.RestResponsePage;
 
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 public class Reader implements ItemReader<DataObject> {
 
 	private static int counter = 0;
 	List<String> list = new ArrayList<>();
 
+	
 	@Override
 	public DataObject read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
         counter++;
@@ -26,7 +35,7 @@ public class Reader implements ItemReader<DataObject> {
         
 		RestTemplate restTemplate = new RestTemplate();
 		
-        String result = restTemplate.getForObject(uri, String.class);
+        RestResponsePage<Vers> result = restTemplate.getForObject(uri, RestResponsePage.class);
 
         System.out.println(result);
 
