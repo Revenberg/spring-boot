7d74f683d077a54b8b13f9ca7cbba856b15843cf
Sander Revenberg <sander@revenberg.info>
2020-02-26 21:21:44 +0100

    .

--- domain/src/main/java/info/revenberg/domain/line/FindLinesInImage.java
+++ domain/src/main/java/info/revenberg/domain/line/FindLinesInImage.java
@@ -207,13 +207,13 @@ public class FindLinesInImage {
         }
 
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println((int) (showBlock.getX1() * scaling));
-        System.out.println((int) ((showBlock.getY1() - 25) * scaling));
-        System.out.println((int) ((showBlock.getX2() - showBlock.getX1()) * scaling));
-        System.out.println((int) ((showBlock.getY2() - showBlock.getY1() + 15) * scaling));
+        System.out.println("1: " + Integer.toString((int) (showBlock.getX1() * scaling)));
+        System.out.println("2: " + Integer.toString((int) ((showBlock.getY1() - 25) * scaling)));
+        System.out.println("3: " + Integer.toString((int) ((showBlock.getX2() - showBlock.getX1()) * scaling)));
+        System.out.println("4: " + Integer.toString((int) ((showBlock.getY2() - showBlock.getY1() + 15) * scaling)));
 
-        System.out.println(myImage.getHeight());
-        System.out.println(myImage.getHeight());
+        System.out.println("5: " + Integer.toString(myImage.getWidth()));
+        System.out.println("6: " + Integer.toString(myImage.getHeight()));
 
         return myImage.getSubimage((int) (showBlock.getX1() * scaling), (int) ((showBlock.getY1() - 25) * scaling),
                 (int) ((showBlock.getX2() - showBlock.getX1()) * scaling),
