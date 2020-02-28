d2ebf2d468c4b720ef7f4d9c9507c493e862b3dc
Sander Revenberg <sander@revenberg.info>
2020-02-28 13:43:41 +0100

    .

--- domain/src/main/java/info/revenberg/domain/line/FindLinesInImage.java
+++ domain/src/main/java/info/revenberg/domain/line/FindLinesInImage.java
@@ -226,7 +226,9 @@ public class FindLinesInImage {
     public void saveImages(String path, String filename) throws IOException {
         BufferedImage img;
         for (int i = 0; i < this.getversLines(); i++) {
+            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA");
             img = this.line(i + 1);
+            System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBB");
             String filepath = path + "/" + filename + "_" + Integer.toString(i + 1) + ".jpeg";
             ImageDefinition imageDefinition = new ImageDefinition((Integer) i);
 
