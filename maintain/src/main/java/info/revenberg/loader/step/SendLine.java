21151ac68f1a752a20ff920587967f6a7a72a7ab
Sander Revenberg <sander@revenberg.info>
2020-04-01 12:46:14 +0200

    .

--- maintain/src/main/java/info/revenberg/loader/step/SendLine.java
+++ maintain/src/main/java/info/revenberg/loader/step/SendLine.java
@@ -16,6 +16,7 @@ public class SendLine {
     RestTemplate restTemplate;
 
     public Line createPost(Line line) {
+        System.out.println("createPost 1");
         String url = "http://40.122.30.210:8090/rest/v1/line";
     
         // create headers
@@ -24,12 +25,12 @@ public class SendLine {
         headers.setContentType(MediaType.APPLICATION_JSON);
         // set `accept` header
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-    
+        System.out.println("createPost 2");
         // send POST request
         ResponseEntity<Line> response = this.restTemplate.postForEntity(url, line, Line.class);
-    
+        System.out.println("createPost 3");
         System.out.println(response);
-
+        System.out.println("createPost 4");
         // check response status code
         if (response.getStatusCode() == HttpStatus.CREATED) {
             return response.getBody();
