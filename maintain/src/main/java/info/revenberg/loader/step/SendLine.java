07f46b5c00b6567b1a50e1b2aebc84436d34c1f2
Sander Revenberg <sander@revenberg.info>
2020-04-01 12:37:12 +0200

    .

--- maintain/src/main/java/info/revenberg/loader/step/SendLine.java
+++ maintain/src/main/java/info/revenberg/loader/step/SendLine.java
@@ -6,12 +6,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
-@Service
 public class SendLine {
 
     @Autowired
