5b614daea5ff1b56ae284475cea98e47a91de18c
Sander Revenberg <sander@revenberg.info>
2020-05-17 20:31:16 +0200

    .

--- maintain/src/main/java/info/revenberg/loader/step/Processor.java
+++ maintain/src/main/java/info/revenberg/loader/step/Processor.java
@@ -15,26 +15,29 @@ public class Processor implements ItemProcessor<Vers, DataObject> {
 
     @Value("${media.temp}")
     private String mediaTempLocation;
-       
+
     @Override
     public synchronized DataObject process(final Vers vers) throws Exception {
+        System.out.println("process start");
+        System.out.println(vers);
+
         if (vers == null) {
             return null;
         }
         DataObject rc = new DataObject();
-        
-        try {
-            //System.out.println("process A");
-            //System.out.println(vers);
-            String uri = "http://40.122.30.210:8090/rest/v1/vers/" + Long.toString(vers.getId()) + "/image";
-            //System.out.println(uri);
 
-            //mediaLocation = "D:/Songs/temp";
+        try {
+            System.out.println("process A");
+            // System.out.println(vers);
+            String uri = "http://40.122.30.210:8090/rest/v1/vers/" + Long.toString(vers.getId()) + "/image";
+            // System.out.println(uri);
+
+            // mediaLocation = "D:/Songs/temp";
             mediaTempLocation = "/var/songs/temp";
             FindLinesInImage images = new FindLinesInImage(uri, mediaTempLocation + "/vers",
                     vers.getSong().getBundle().getName(), vers.getSong().getName(), vers.getSong().getId());
 
-            //System.out.println("process B");
+            // System.out.println("process B");
 
             for (Map.Entry<Integer, ImageDefinition> entry : images.getImageDefinitions().entrySet()) {
                 ImageDefinition imageDefinition = entry.getValue();
@@ -50,13 +53,13 @@ public class Processor implements ItemProcessor<Vers, DataObject> {
                 System.out.println(line);
 
                 rc.add(line);
-                System.out.println( imageDefinition.getTitle() );
+                System.out.println(imageDefinition.getTitle());
                 System.out.println("@@@@@@@@@@@@@@@@@@@@ c @@@@@@@@@@@@@@@@@@@@@");
             }
         } catch (Exception e) {
-            System.out.println("===========Exception ============");            
+            System.out.println("===========Exception ============");
             System.out.println(vers);
-            System.out.println(mediaTempLocation);            
+            System.out.println(mediaTempLocation);
             System.out.println(e);
             System.out.println(e.getMessage());
             System.out.println(e.getLocalizedMessage());
--- maintain/src/main/java/info/revenberg/loader/step/Reader.java
+++ maintain/src/main/java/info/revenberg/loader/step/Reader.java
@@ -12,10 +12,11 @@ import info.revenberg.domain.Vers;
 
 public class Reader implements ItemReader<Vers> {
 
-	private static Long lastID = 0L;	
+	private static Long lastID = 0L;
 
 	@Override
-	public synchronized Vers read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+	public synchronized Vers read()
+			throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
 		System.out.println(Long.toString(lastID) + "synchronized !!!!!!!!!!!!!!!! a !!!!!!!!!!!");
 
 		String uri = "http://40.122.30.210:8090/rest/v1/vers/" + Long.toString(lastID) + "/next";
@@ -23,55 +24,56 @@ public class Reader implements ItemReader<Vers> {
 		RestTemplate restTemplate = new RestTemplate();
 
 		Long id = restTemplate.getForObject(uri, Long.class);
-		//System.out.println(Long.toString(lastID) + "!!!!!!!!!!!!! a !!!!!!!!!!!!!!");
+		// System.out.println(Long.toString(lastID) + "!!!!!!!!!!!!! a !!!!!!!!!!!!!!");
 		if (id == null) {
 			return null;
-		}	
+		}
 		if (lastID == id) {
 			return read();
 		}
 		lastID = id;
-		
+
 		uri = "http://40.122.30.210:8090/rest/v1/vers/" + Long.toString(id);
+		System.out.println(Long.toString(lastID) + "!!!!!!!!!!!!!!!! b0 !!!!!!!!!!!");
+		System.out.println(uri);
 		Vers vers = restTemplate.getForObject(uri, Vers.class);
-		//System.out.println(vers);
-		System.out.println(Long.toString(lastID) + "!!!!!!!!!!!!!!!! b !!!!!!!!!!!");
+		// System.out.println(vers);
+		System.out.println(Long.toString(lastID) + "!!!!!!!!!!!!!!!! b1 !!!!!!!!!!!");
 		TimeUnit.SECONDS.sleep(15);
 		return vers;
 
 		/*
-		RestResponsePage pages = restTemplate.getForObject(uri, RestResponsePage.class);
-	
-		List v = pages.getContent();
-		LinkedHashMap c = (LinkedHashMap) v.get(0);
-		System.out.println(Integer.toString(counter) + "!!!!!!!!!!!!! a !!!!!!!!!!!!!!");
-		System.out.println(c);
-		System.out.println(c.getClass());
-		System.out.println(c.getClass().getSimpleName());
-		System.out.println(c.size());
-		System.out.println(Integer.toString(counter) + "!!!!!!!!!!!!!!!! b !!!!!!!!!!!");
-		*/
+		 * RestResponsePage pages = restTemplate.getForObject(uri,
+		 * RestResponsePage.class);
+		 * 
+		 * List v = pages.getContent(); LinkedHashMap c = (LinkedHashMap) v.get(0);
+		 * System.out.println(Integer.toString(counter) +
+		 * "!!!!!!!!!!!!! a !!!!!!!!!!!!!!"); System.out.println(c);
+		 * System.out.println(c.getClass());
+		 * System.out.println(c.getClass().getSimpleName());
+		 * System.out.println(c.size()); System.out.println(Integer.toString(counter) +
+		 * "!!!!!!!!!!!!!!!! b !!!!!!!!!!!");
+		 */
 
-		/*if (c.isPresent()) {
-			System.out.println(Integer.toString(counter) + "!!!!!!!!!!!!! a !!!!!!!!!!!!!!");
-			System.out.println(c.get());
-			System.out.println(c.get().getClass());
-			System.out.println(c.get().getClass().getName());
-			System.out.println(Integer.toString(counter) + "!!!!!!!!!!!!!!!! b !!!!!!!!!!!");
-			return (Vers) c.get();
-		}
-*/
-		
-//		Vers vers = result.get(0);
-//		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-//		System.out.println(vers.getId());
-//		System.out.println(vers.getLocation());
+		/*
+		 * if (c.isPresent()) { System.out.println(Integer.toString(counter) +
+		 * "!!!!!!!!!!!!! a !!!!!!!!!!!!!!"); System.out.println(c.get());
+		 * System.out.println(c.get().getClass());
+		 * System.out.println(c.get().getClass().getName());
+		 * System.out.println(Integer.toString(counter) +
+		 * "!!!!!!!!!!!!!!!! b !!!!!!!!!!!"); return (Vers) c.get(); }
+		 */
+
+		// Vers vers = result.get(0);
+		// System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		// System.out.println(vers.getId());
+		// System.out.println(vers.getLocation());
 
 		/*
 		 * if (!list.isEmpty()) { String element = list.get(0); list.remove(0); return
 		 * new Vers(element); }
 		 */
-		
+
 	}
 
 }
