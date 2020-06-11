b7de981a44d2713f3596d74b5876fc9601ecc0fe
Sander Revenberg <sander@revenberg.info>
2020-06-11 19:40:50 +0200

    .

--- maintain/src/main/java/info/revenberg/loader/step/Reader.java
+++ maintain/src/main/java/info/revenberg/loader/step/Reader.java
@@ -4,15 +4,72 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+import java.util.List;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Reader implements ItemReader<Long> {
 
+	@Autowired
+    private JobExplorer jobExplorer;
+    @Autowired
+    JobRepository jobRepository;
+    //@Autowired
+    //private JobLauncher jobLauncher;
+    @Autowired 
+	JobOperator jobOperator;
+	
 	private static Long lastID = 0L;
 
 	@Override
 	public synchronized Long read()
-			throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {		
+			throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {	
+				
+				
+				try {
+					System.out.println(jobExplorer.getJobNames());
+
+					List<JobInstance> jobInstances = jobExplorer.getJobInstances("jobName",0,1);// this will get one latest job from the database
+					if(CollectionUtils.isNotEmpty(jobInstances)){						
+					   JobInstance jobInstance =  jobInstances.get(0);
+					   System.out.println(jobInstance);					
+					   List<JobExecution> jobExecutions = jobExplorer.getJobExecutions(jobInstance);
+					   if(CollectionUtils.isNotEmpty(jobExecutions)){
+						   for(JobExecution execution: jobExecutions){
+							   // If the job status is STARTED then update the status to FAILED and restart the job using JobOperator.java
+							   if(execution.getStatus().equals(BatchStatus.STARTED)){ 
+								System.out.println(execution);					
+								   execution.setEndTime(new Date());
+								   execution.setStatus(BatchStatus.FAILED);                               
+								   execution.setExitStatus(ExitStatus.FAILED);                               
+								   jobRepository.update(execution);
+								   jobOperator.restart(execution.getId());
+							   }
+						   }
+					   }
+					}
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+
+
+
+
+
+				
+				
 		String uri = "http://40.122.30.210:8090/rest/v1/vers/" + Long.toString(lastID) + "/next";
 
 		RestTemplate restTemplate = new RestTemplate();
