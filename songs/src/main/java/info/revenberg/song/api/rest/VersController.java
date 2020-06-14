bbae2eeae1746d302d2c0b2334b11fa40a23818c
Sander Revenberg <sander@revenberg.info>
2020-06-14 15:41:52 +0200

    .

--- songs/src/main/java/info/revenberg/song/api/rest/VersController.java
+++ songs/src/main/java/info/revenberg/song/api/rest/VersController.java
@@ -116,19 +116,19 @@ public class VersController extends AbstractRestHandler {
 
                                         this.lineService.createLine(line);
                                         line = null;
-                                        Runtime runtime = Runtime.getRuntime();
-
-                                        NumberFormat format = NumberFormat.getInstance();
-                        
-                                        long maxMemory = runtime.maxMemory();
-                                        long allocatedMemory = runtime.totalMemory();
-                                        long freeMemory = runtime.freeMemory();
-                        
-                                        System.out.println("free memory: " + format.format(freeMemory / 1024) );
-                                        System.out.println("allocated memory: " + format.format(allocatedMemory / 1024) );
-                                        System.out.println("max memory: " + format.format(maxMemory / 1024) );
-                                        System.out.println("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) );                                        
                                 }
+                                Runtime runtime = Runtime.getRuntime();
+                                NumberFormat format = NumberFormat.getInstance();
+
+                                long maxMemory = runtime.maxMemory();
+                                long allocatedMemory = runtime.totalMemory();
+                                long freeMemory = runtime.freeMemory();
+
+                                System.out.println("free memory: " + format.format(freeMemory / 1024));
+                                System.out.println("allocated memory: " + format.format(allocatedMemory / 1024));
+                                System.out.println("max memory: " + format.format(maxMemory / 1024));
+                                System.out.println("total free memory: "
+                                                + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024));
                         } catch (Exception e) {
                                 throw new DataFormatException("Failing creating files");
                         }
