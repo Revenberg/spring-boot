f12434134f3fb05f429a92ece7c45dc8e762a659
Sander Revenberg <sander@revenberg.info>
2020-06-14 15:40:26 +0200

    .

--- songs/src/main/java/info/revenberg/song/api/rest/VersController.java
+++ songs/src/main/java/info/revenberg/song/api/rest/VersController.java
@@ -124,10 +124,10 @@ public class VersController extends AbstractRestHandler {
                                         long allocatedMemory = runtime.totalMemory();
                                         long freeMemory = runtime.freeMemory();
                         
-                                        System.out.println("free memory: " + format.format(freeMemory / 1024) + "<br/>");
-                                        System.out.println("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
-                                        System.out.println("max memory: " + format.format(maxMemory / 1024) + "<br/>");
-                                        System.out.println("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");                                        
+                                        System.out.println("free memory: " + format.format(freeMemory / 1024) );
+                                        System.out.println("allocated memory: " + format.format(allocatedMemory / 1024) );
+                                        System.out.println("max memory: " + format.format(maxMemory / 1024) );
+                                        System.out.println("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) );                                        
                                 }
                         } catch (Exception e) {
                                 throw new DataFormatException("Failing creating files");
