bc40014449c87c71a1c3e63bae4708f09e5db8d6
Sander Revenberg <sander@revenberg.info>
2020-06-14 15:39:23 +0200

    .

--- maintain/src/main/java/info/revenberg/loader/step/Reader.java
+++ maintain/src/main/java/info/revenberg/loader/step/Reader.java
@@ -65,16 +65,14 @@ public class Reader implements ItemReader<Long> {
 
 		NumberFormat format = NumberFormat.getInstance();
 
-		StringBuilder sb = new StringBuilder();
 		long maxMemory = runtime.maxMemory();
 		long allocatedMemory = runtime.totalMemory();
 		long freeMemory = runtime.freeMemory();
 
-		sb.append("free memory: " + format.format(freeMemory / 1024) + "<br/>");
-		sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
-		sb.append("max memory: " + format.format(maxMemory / 1024) + "<br/>");
-		sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");
-		System.out.println(sb.toString());
+		System.out.println("free memory: " + format.format(freeMemory / 1024) + "<br/>");
+		System.out.println("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
+		System.out.println("max memory: " + format.format(maxMemory / 1024) + "<br/>");
+		System.out.println("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");
 		return lastID;
 
 	}
--- songs/src/main/java/info/revenberg/song/api/rest/VersController.java
+++ songs/src/main/java/info/revenberg/song/api/rest/VersController.java
@@ -120,16 +120,14 @@ public class VersController extends AbstractRestHandler {
 
                                         NumberFormat format = NumberFormat.getInstance();
                         
-                                        StringBuilder sb = new StringBuilder();
                                         long maxMemory = runtime.maxMemory();
                                         long allocatedMemory = runtime.totalMemory();
                                         long freeMemory = runtime.freeMemory();
                         
-                                        sb.append("free memory: " + format.format(freeMemory / 1024) + "<br/>");
-                                        sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
-                                        sb.append("max memory: " + format.format(maxMemory / 1024) + "<br/>");
-                                        sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");
-                                        System.out.println(sb.toString());
+                                        System.out.println("free memory: " + format.format(freeMemory / 1024) + "<br/>");
+                                        System.out.println("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
+                                        System.out.println("max memory: " + format.format(maxMemory / 1024) + "<br/>");
+                                        System.out.println("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");                                        
                                 }
                         } catch (Exception e) {
                                 throw new DataFormatException("Failing creating files");
